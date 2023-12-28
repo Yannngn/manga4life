5a79f321495ce7cfbf06c77b2e46f43e60304bab
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2023-12-28 15:59:17 -0300

    add sleep

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ from PIL import Image
 
 class Manga:
     xml_url = "https://manga4life.com/rss"
-    image_url = "https://temp.compsci88.com/manga"
+    # image_url = "https://temp.compsci88.com/manga"
+    image_url = "https://scans-hot.leanbox.us/manga"
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, path: str | None = None) -> None:
         self.name = name
         self.uid = name.capitalize().replace(" ", "-")
         self.slug = name.lower().replace(" ", "_")
 
-        self.dir = os.path.join("data", self.slug)
+        self.path = os.path.join(path or "data", self.slug)
 
-        logging.basicConfig(
-            filename=f"{self.slug}.log", encoding="utf-8", level=logging.DEBUG
-        )
+        self.set_logger()
+
+    def set_logger(self):
+        file_handler = logging.FileHandler(f"{self.slug}.log", encoding="utf-8")
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+
+        self.logger = logging.getLogger()
+        self.logger.addHandler(file_handler)
+
+        self.logger.setLevel(logging.DEBUG)
 
     def find_last_chapter(self) -> int:
         url_to_fetch = f"{self.xml_url}/{self.uid}.xml"
@@ -52,28 +61,28 @@ class Manga:
                     response.raise_for_status()
 
                 except aiohttp.ClientResponseError as e:
-                    logging.warning(f"Error in chapter {chapter}, page {page}: {e}")
+                    self.logger.warning(f"Error in chapter {chapter}, page {page}: {e}")
                     raise e
 
                 image = Image.open(BytesIO(await response.read()))
                 image.save(
-                    os.path.join(self.dir, str(chapter).zfill(4), f"{page:03}.png")
+                    os.path.join(self.path, str(chapter).zfill(4), f"{page:03}.png")
                 )
 
-                logging.info(f"'{image_url}' downloaded successfully")
+                self.logger.info(f"'{image_url}' downloaded successfully")
 
-    async def _download_pages_in_chunks(self, chapter, window_size):
+    async def _download_pages_in_chunks(self, chapter: int, window_size: int):
         page = 1
         while True:
             chunk = range(page, page + window_size)
             yield [self.download_page(chapter, page) for page in chunk]
             page += window_size
 
-    async def download_chapter(self, chapter: int, window: int = 5):
-        dir_path = os.path.join(self.dir, str(chapter).zfill(4))
+    async def download_chapter(self, chapter: int, window_size: int = 5):
+        dir_path = os.path.join(self.path, str(chapter).zfill(4))
         os.makedirs(dir_path, exist_ok=True)
 
-        async for page_group in self._download_pages_in_chunks(chapter, window):
+        async for page_group in self._download_pages_in_chunks(chapter, window_size):
             try:
                 await asyncio.gather(*page_group)
             except aiohttp.ClientResponseError as e:
@@ -86,26 +95,28 @@ class Manga:
 
             yield [self.download_chapter(chapter) for chapter in chunk]
 
-    async def download_all_chapters(
-        self, begin: int = 1, end: int = -1, window: int = 10
+    async def download_chapters(
+        self, begin: int = 1, end: int = -1, window_size: int = 10
     ):
         if end == -1:
             end = self.find_last_chapter()
 
         chapters = range(begin, end + 1)
-        async for chapter_group in self._download_chapters_in_chunks(chapters, window):
+        async for chapter_group in self._download_chapters_in_chunks(
+            chapters, window_size
+        ):
             await asyncio.gather(*chapter_group)
+            self.logger.info(f"{len(chapter_group)} chapters downloaded")
 
-            logging.info(f"{len(chapter_group)} chapters downloaded")
+            self.logger.debug(f"sleeping for 300 seconds")
+            await asyncio.sleep(300)
 
-            logging.debug(f"sleeping for 30 seconds")
-
-            await asyncio.sleep(30)
+        self.logger.debug("done")
 
 
 async def main():
     m = Manga(name)
-    await m.download_all_chapters(45)
+    await m.download_chapters(165)
 
 
 if __name__ == "__main__":
